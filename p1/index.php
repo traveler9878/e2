225b864f691f7e738e25ebe5994b2e3bb4917ad8
David Curtis <traveler9878@gmail.com>
2022-09-28 23:42:42 +0000

    Works veresion 1

--- p1/index.php
+++ p1/index.php
@@ -1,37 +1,36 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <title>Project 1 (David Curtis)</title>
-    <style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
+//create two dice
+$die_1 = [1,2,3,4,5,6];
+$die_2 = [1,2,3,4,5,6];
+
+$initial_roll_die_1 = $die_1[rand(0, 5)];
+$initial_roll_die_2 = $die_2[rand(0,5)];
+
+$initial_roll = $initial_roll_die_1 + $initial_roll_die_2;
+
+//initialize $winner as an integer and test type to see if the game is over
+$winner = 1;
+
+//initialize $results to hold the array of roll results
+$results = [$initial_roll];
+
+if($initial_roll == 7 or $initial_roll ==11){
+    $winner = true;
+}elseif($initial_roll == 2 or $initial_roll == 3 or $initial_roll == 12){
+    $winner = false;
+}
+
+while (gettype($winner) == 'integer'){
+    $d1 = $die_1[rand(0,5)];
+    $d2 = $die_2[rand(0,5)];
+    $roll = $d1 + $d2;
+    array_push($results, $roll);
+    if ($roll == 7){
+        $winner = false;
+    }elseif($roll == $initial_roll){
+        $winner = true;
     }
-    </style>
-</head>
+}
 
-<body>
-    <h1>Project 1</h1>
-
-    <h2>Mechanics</h2>
-    <ul>
-        <li>...</li>
-
-    </ul>
-
-    <h2>Mechanics</h2>
-    <ul>
-        <li>...</li>
-
-    </ul>
-
-    <h2>Mechanics</h2>
-    <ul>
-        <li>...</li>
-
-    </ul>
-</body>
-
-</html>
+require 'index-view.php';
