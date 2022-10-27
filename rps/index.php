8cde8920619074502eb44352a6a1fa4d3dfd8a68
David Curtis <traveler9878@gmail.com>
2022-10-27 03:01:54 +0000

    wrapped play in var_dump

--- rps/index.php
+++ rps/index.php
@@ -6,4 +6,4 @@ use RPS\Game;
 $game = new Game();
 
 # Each invocation of the `play` method will play and track a new round of player (given move) vs. computer
-$game->play('rock');
+var_dump($game->play('rock'));
